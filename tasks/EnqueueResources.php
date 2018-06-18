0593f18669e3d3620a6c035bc6e511a05fa68695
Jalil <jalil@sehrgute.software>
2018-06-18 16:23:40 +0200

    Upgrade to stable bootstrap version; Simplify build process via webpack

--- tasks/EnqueueResources.php
+++ tasks/EnqueueResources.php
@@ -22,7 +22,7 @@ class EnqueueResources extends Task
 
     protected $scripts = [
         'theme-lib' => [
-            'path' => 'static/js/main.js',
+            'path' => 'static/js/bundle.js',
             'version' => '1.0',
             'dependencies' => [],
             'in_footer' => true,
@@ -33,34 +33,41 @@ class EnqueueResources extends Task
     {
         foreach ($this->scripts as $handle => $script) {
             $path = sprintf('%s/%s', get_stylesheet_directory_uri(), $script['path']);
-            wp_enqueue_script($handle, $path, $script['dependencies'],
-                              $script['version'], $script['in_footer']);
+            wp_enqueue_script(
+                $handle,
+                $path,
+                $script['dependencies'],
+                              $script['version'],
+                $script['in_footer']
+            );
         }
     }
 
     /**
-	 * Required necessary external css files for the theme.
-	 *
-	 * @return void
-	 */
-	public function  enqueueStyles()
-	{
-		wp_enqueue_style(
-			'fontawesome',
-			'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-			null,
-			'4.7.0'
-		);
-	}
+     * Required necessary external css files for the theme.
+     *
+     * @return void
+     */
+    public function enqueueStyles()
+    {
+        wp_enqueue_style(
+            'fontawesome',
+            'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+            null,
+            '4.7.0'
+        );
+    }
 
     /**
     * Limit max menu depth in admin panel to 2
     *
     * @return void
     */
-    public function  limitMenuDepth($hook)
+    public function limitMenuDepth($hook)
     {
-        if ($hook != 'nav-menus.php') return;
+        if ($hook != 'nav-menus.php') {
+            return;
+        }
 
         // override default value right after 'nav-menu' JS
         wp_add_inline_script('nav-menu', 'wpNavMenu.options.globalMaxDepth = 1;', 'after');
